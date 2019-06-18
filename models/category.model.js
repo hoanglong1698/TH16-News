ad0af062097146f0f7742687f8f5bbee792ee65a
Hoang Long <1612352@student.hcmus.edu.vn>
2019-06-18 17:09:24 +0700

    Thêm hàm loadactive ở categorymodel

--- models/category.model.js
+++ models/category.model.js
@@ -5,6 +5,10 @@ module.exports = {
     return db.load('select * from loaitin');
   },
 
+  loadactive: () => {
+    return db.load(`select * from theloai where AnHien = ${1}`);
+  },
+
   editor: () => {
     return db.load('select * from editors');
   },
@@ -12,6 +16,16 @@ module.exports = {
   add: (category) => {
     var sql = `insert into loaitin (Ten, BienTapVien, ThuTu, NgayTao, idTL)
                values ('${category.CategoryName}', '${category.EditorName}', '20', '19-06-2019', '1')`
+    return db.load(sql);
+  },
+
+  edit: (category) => {
+    var sql = `UPDATE loaitin SET Ten = '${category.TagName}',  WHERE idTag = '${tag.idTag}'`
+    return db.load(sql);
+  },
+
+  delete: (tag) => {
+    var sql = `DELETE from tags WHERE idTag = '${tag.idTag}'`
     return db.load(sql);
   },
 };
