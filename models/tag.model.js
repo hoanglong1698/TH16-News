1c87b2db73ee385bfbc6db6f4bb6ceb5209ee9f7
Hoang Long <1612352@student.hcmus.edu.vn>
2019-06-18 14:24:11 +0700

    Thêm tag model

---
+++ models/tag.model.js
@@ -0,0 +1,27 @@
+var db = require('../utils/db');
+
+module.exports = {
+  all: () => {
+    return db.load('select * from tags');
+  },
+
+  single: id => {
+    return db.load(`select * from tags where idTag = '${id}'`)
+  },
+
+  add: (tag) => {
+    var sql = `insert into tags (TenTag, NgayTao)
+               values ('${tag.TagName}', '${tag.NgayTao}')`
+    return db.load(sql);
+  },
+
+  edit: (tag) => {
+    var sql = `UPDATE tags SET TenTag = '${tag.TagName}' WHERE idTag = '${tag.idTag}'`
+    return db.load(sql);
+  },
+
+  delete: (tag) => {
+    var sql = `DELETE from tags WHERE idTag = '${tag.idTag}'`;
+    return db.load(sql);
+  },
+};
