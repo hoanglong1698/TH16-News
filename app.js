3f3f0eba2de8618b7bae1c991407411df6b1ac37
Kong Kiet <vokongkiet@gmail.com>
2019-06-23 21:54:58 +0700

    Cập nhật app.js cho các chức năng xem bài viết

--- app.js
+++ app.js
@@ -1,52 +1,35 @@
 var express = require('express');
-var exphbs = require('express-handlebars');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
-// parse application/x-www-form-urlencoded
-app.use(express.json()) 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: true }))
 
+require('./middlewares/view-engine')(app);
+require('./middlewares/session')(app);
+require('./middlewares/passport')(app);
 
-// parse application/json
-app.use(bodyParser.json())
-app.engine('hbs', exphbs({
-    layoutsDir: 'views/_layouts',
-    defaultLayout: 'main.hbs'
-  }));
-  app.set('view engine', 'hbs');
+app.use(require('./middlewares/locals.mdw'));
+app.use(require('./middlewares/auth-locals.mdw'));
 
-app.get('/admin/adduser', (req, res) => {
-    res.render('adduser');
-})
-app.get('/admin/deleteuser', (req, res) => {
-    res.render('deleteuser');
-})
-
-app.get('/admin/edituser', (req, res) => {
-    res.render('edituser');
-})
-app.get('/admin/updateadmin', (req, res) => {
-    res.render('updateadmin');
-})
 app.get('/admin', (req, res) => {
-    res.render('adminmanage');
+    res.render('admin/vwDashboard/index', {
+        layout : 'main'
+    });
 })
 
-app.get('/', (req, res) => {
-    res.render('admin/vwDashboard/index');
-})
-
+//app.use('/admin', require('./routes/admin/users.route'));
+app.use('/', require('./routes/Home.route'));
+app.use('/account', require('./routes/account.route'));
+app.use('/categories', require('./routes/Article.route')); //chứa danh sách bài viết , và chi tiết bài viết
 app.use('/admin/categories', require('./routes/admin/category.route'));
 app.use('/admin/user', require('./routes/admin/users.route'));
 app.use('/admin/tag', require('./routes/admin/tag.route'));
 app.use('/admin/post_create', require('./routes/admin/post_create.route'));
 app.use('/admin/post_approve', require('./routes/admin/post_approve.route'));
 app.use('/admin/post_manage', require('./routes/admin/post_manage.route'));
-
 app.listen(3000, () => {
     console.log('Web Server is running at http://localhost:3000');
 })
